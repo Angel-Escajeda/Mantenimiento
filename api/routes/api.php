580588f78c8963a9526c05072a4298c818fb8f9f
Angel-Escajeda <01100001.01100101@protonmail.com>
2020-09-24 09:10:38 -0500

    Mod DB y API getcatalogo

--- api/routes/api.php
+++ api/routes/api.php
@@ -15,14 +15,12 @@ Route::post('login'          ,'userController@IniciarSesion')   -> name('Iniciar
 Route::post('login.permisos' ,'userController@SesionPermisos')  -> name('SesionPermisos');
 
 //=================================== USUARIOS======================================================
-Route::get('catusuarios'     ,'userController@getcatalogo') -> name('getcatalogo');   
+Route::get('catusuarios'     ,'userController@getcatalogo') -> name('getcatalogo');
 Route::get('usuarios'	     ,'userController@getAll')      -> name('getAllUsuarios');  //PROBADA
 Route::post('usuarios'       ,'userController@add')         -> name('addUsuarios');    	//PROBADA
 Route::put('usuarios/{id}'   ,'userController@update')  	-> name('updateUsuarios');  //PROBADA
 Route::delete('usuarios/{id}','userController@delete')      -> name('deleteUsuarios');  //PROBADA
 Route::post('estatus.user'   ,'userController@estatusUser') -> name('estatusUser');    	//PROBADA
-
-/
 
 
 
